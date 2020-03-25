b323037165c6ec9e60c5bbdf3422e0a018919f9e
tinpl <tinpl@DESKTOP-KAMEKU>
2020-03-25 20:15:43 +0100

    Mi primer repositoriooOOOOoOoO

---
+++ src/Printear.java
@@ -0,0 +1,9 @@
+
+public class Printear {
+    static String textoCaracteresTotal (int total) {
+return ("Total caracteres: "+total);
+}
+static String textoNumeroPalabras (int total) {
+return ("Total palabras: "+total);
+}
+}
