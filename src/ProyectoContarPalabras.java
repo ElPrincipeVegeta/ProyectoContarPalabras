1541637857f459de58038eaedfddff31bd3d6efa
tinpl <tinpl@DESKTOP-KAMEKU>
2020-03-25 20:18:20 +0100

    Mi primer repositoriooOOOOoOoO

--- src/ProyectoContarPalabras.java
+++ src/ProyectoContarPalabras.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class ProyectoContarPalabras {
 Scanner sc = new Scanner(System.in);
 public static void main (String []args) {
+    
+   
+ContarPalabras c1 = new ContarPalabras();
+Printear p1 = new Printear();
+    
 Scanner sc = new Scanner(System.in);
 System.out.print("Introduce un string: ");
 String cadenilla = sc.nextLine();
-System.out.println("\n"+textoCaracteresTotal (contarCaracteresTotal(cadenilla))+"\n");
-System.out.println(textoNumeroPalabras(contarNumeroPalabras(cadenilla))+"\n");
+System.out.println("\n"+p1.textoCaracteresTotal (c1.contarCaracteresTotal(cadenilla))+"\n");
+System.out.println(p1.textoNumeroPalabras(c1.contarNumeroPalabras(cadenilla))+"\n");
 System.out.println("Repeticiones caracteres: \n");
-repeticionesPro(cadenilla);
-}
-
-static String textoCaracteresTotal (int total) {
-return ("Total caracteres: "+total);
-}
-static String textoNumeroPalabras (int total) {
-return ("Total palabras: "+total);
+c1.repeticionesPro(cadenilla);
 }
 }
