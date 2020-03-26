9b8ebb74d09e6a30061570b46b62e91bca62d335
tinpl <tinpl@DESKTOP-KAMEKU>
2020-03-26 15:06:23 +0100

    Los JUnit

--- src/Printear.java
+++ src/Printear.java
@@ -1,9 +1,38 @@
-
+/**
+ * @author alumno
+ * @version 1.0.14
+ *
+ *La clase ContarPalabras contiene los métodos necesarios para contabilizar el numero de palabras,
+ *así como contar el numero de caracteres. Además puede también contar cuantas veces se repite cada caracter.
+ * 
+ */
 public class Printear {
-    static String textoCaracteresTotal (int total) {
+
+/**
+ * @param textoCaracteresTotal
+ * 
+ * Método que devuelve una cadena de texto donde no informa el total de caracteres contados.
+ * 
+ * @return cadena devueve la cadena de texto.
+ * 
+* */        
+    
+    
+String textoCaracteresTotal (int total) {
 return ("Total caracteres: "+total);
 }
-static String textoNumeroPalabras (int total) {
+
+
+/**
+ * @param textoNumeroPalabras
+ * 
+ * Método que devuelve una cadena de texto donde no informa el total de palabras contadas.
+ * 
+ * @return cadena devueve la cadena de texto.
+ * 
+* */        
+
+String textoNumeroPalabras (int total) {
 return ("Total palabras: "+total);
 }
 }
