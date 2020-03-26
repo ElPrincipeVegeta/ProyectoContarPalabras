aa1a418457dc4eb61e3f52aeedd6ce85302d6dcf
tinpl <tinpl@DESKTOP-KAMEKU>
2020-03-26 15:06:51 +0100

    Los JUnit

--- src/ContarPalabras.java
+++ src/ContarPalabras.java
@@ -1,8 +1,27 @@
 
 import java.util.HashMap;
 
+/**
+ * @author alumno
+ * @version 1.0.14
+ *
+ *La clase ContarPalabras contiene los métodos necesarios para contabilizar el numero de palabras,
+ *así como contar el numero de caracteres. Además puede también contar cuantas veces se repite cada caracter.
+ * 
+ */
 public class ContarPalabras {
-    static int contarNumeroPalabras(String cadena) { //cuenta el número de palabras introducido.
+
+/**
+ * @param contarNumeroPalabras
+ * 
+ * Método que cuenta el número de palabras introducido.
+ * 
+ * @return pal devuelve el contador de palabras.
+ * 
+* */    
+    
+    
+int contarNumeroPalabras(String cadena) { //cuenta el número de palabras introducido.
 int pal=1;
 for(int i=0;i<cadena.length();i++) {
 if(cadena.charAt(i)==' ')
@@ -10,7 +29,17 @@ pal++;
 }
 return pal;
 }
-static int contarCaracteresTotal(String cadena) { //devuelve el número total de caracteres dada una determinada cadena.
+
+/**
+ * @param contarCaracteresTotal
+ * 
+ * Método que devuelve el número total de caracteres dada una determinada cadena.
+ * 
+ * @return cont devuelve el contador de caracteres.
+ * 
+* */    
+
+int contarCaracteresTotal(String cadena) { //devuelve el número total de caracteres dada una determinada cadena.
 int cont=0;
 for(int i=0;i<cadena.length();i++) {
 if(cadena.charAt(i)!=' ')
@@ -18,7 +47,17 @@ cont++;
 }
 return cont;
 }
-public static void repeticionesPro(String cad) { //imprime cada caracter con su numero de aparicion
+
+/**
+ * @param repeticionesPro
+ * 
+ * Método que devuelve cuantas veces se repite cada caracter.
+ * 
+ * 
+* */    
+
+
+static void repeticionesPro(String cad) { //imprime cada caracter con su numero de aparicion
 HashMap<Character, Integer> mapa = new HashMap<Character, Integer>();
 for(int i=0; i<cad.length();i++) { //guarda todos los caracteres de la cadena con un valor de 1
 mapa.put(cad.charAt(i), 1);
